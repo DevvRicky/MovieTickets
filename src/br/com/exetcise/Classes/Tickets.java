7ba7bc1dfea64cb1b035eec76ae5bdaa7f6fa0de
Ricky <rickysecundario@gmail.com>
2025-12-03 20:02:13 -0300

    Adding comments

--- src/br/com/exetcise/Classes/Tickets.java
+++ src/br/com/exetcise/Classes/Tickets.java
@@ -13,46 +13,44 @@ public class Tickets {
         this.value = 50;
     }
 
-    //Méthods
-    //If the person is bellow 18, the price will be with 10% Discount
-    //If the person is older than 60, the price will be with 30% Discount
-    //If the person is between 18 and 60, the price will be the full price
+    //Methods
 
     public void discountCalc(){
+        //Quick menu
         System.out.println("TICKET DISCOUNT CALCULATOR"+
                 "-------------------------\n"+
                 "What is your age?");
-        age = 0;
-        while(age < 1){
+        age = 0; //Already defines the age, so it can enter the loop
+        while(age < 1){ //Check if the age is valid
             age = sc.nextInt();
             if (age < 0){
                 System.out.println("Invalid age\n" +  "Try again: ");
             }
         }
-
-        if(age <= 18){
+        //Since its valid, it will calculate the discount based on age
+        if(age <= 18){//Youngsters of 18 or less receive 10% of discount
             value = value - (value *0.1);
             System.out.println("Your Ticket with discount will be: R$" + value);
         }
-        else if (age >= 60){
+        else if (age >= 60){//Elders of 60 or more receive 30% of discount
             value = value - (value *0.3);
             System.out.println("Your Ticket with discount will be: R$" + value);
 
-        }else {
+        }else {//If it's just a normal adult, it's at full price
             System.out.println("Your Ticket will be at full price: R$" + value);
         }
 
         //There is also a discount in case someone want to buy more than one ticket
         System.out.println("How many tickets do you want to buy?");
-        int quantity = sc.nextInt();
+        int quantity = sc.nextInt(); //User enters the quantity they want to buy
 
-        if(quantity <= 5 && quantity > 1){
+        if(quantity <= 5 && quantity > 1){// If it's a small quantity, its 10% of discount
             value = value - (value *0.1);
             double total = quantity * value;
             System.out.println("The price will be R$"+value+" per ticket\n"+
                     "And the total will be R$"+total);
         }
-        else if(quantity >= 6){
+        else if(quantity >= 6){// If it's a large quantity, it's 30% of discount
             value = value - (value *0.3);
             double total = quantity * value;
             System.out.println("The price will be R$"+value+" per ticket\n"+
